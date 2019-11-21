f283fa620c8af14a518367a477407828b2275fac
dwugofski <dwugofsk@cirrus.com>
2019-11-21 14:56:53 -0600

    Starting on the backend

--- inc/p2em_core.h
+++ inc/p2em_core.h
@@ -11,6 +11,10 @@
 #include <map>
 #include <unordered_map>
 #include <stdint.h>
+#include <exception>
+
+#define P2EMON_NULL_ID				0
+#define P2EMON_NULL_NAME_STRING		"__NULL__"
 
 namespace p2em_core {
 
@@ -20,20 +24,91 @@ namespace p2em_core {
 	template<typename T_key_t, typename T_val_t> using umap = std::unordered_map<T_key_t, T_val_t>;
 	typedef unsigned int uint;
 
-	class Creature {
-	private:
-		uint _id;
-		uint _name;
-		uint _strength;
+	template<typename T_valunit_t>
+	struct modvalT {
+		string name;
+		T_valunit_t base;
+		T_valunit_t mod;
+	};
+	using modval = modvalT<uint>;
+
+	enum P2EMonExceptionCode {
+		P2EMON_ERR_INVALID_ID,
+		P2EMON_ERR_INVALID_NAME,
+		P2EMON_ERR_DUPLICATE_MONSTER,
+		P2EMON_ERR_MONSTER_NOT_FOUND,
+		P2EMON_ERR_MONSTER_MISMATCH,
+		P2EMON_ERR_ID_CREATION_TIMEOUT,
+
+		P2EMON_ERR
+	};
+
+	const string P2EMonExceptionStrings[P2EMON_ERR + 1] = {
+		"Invalid monster name received!",
+		"Invalid monster id received!",
+		"Cannot create duplicate monsters!",
+		"Cannot find monster!",
+		"Monster id does not match recorded id!",
+		"Could not create new monster ID!",
+
+		"Monster error!"
+	};
+
+	class Core;
+	class P2EMonException;
+	class Monster;
+
+	class Core {
+	protected:
+		umap<uint, Monster*> _monsters;
 	public:
-		Creature(uint id);
-		Creature(uint id, string name);
+		Core();
+
+		bool has(uint id) const;
+		bool has(const Monster* monster) const;
+		const Monster* get(uint id) const;
+
+		void add(Monster* monster);
+		void remove(Monster* monster);
+		void remove(uint id);
+		Monster* get(uint id);
+
+		const Monster* operator[](uint id) const;
+		Monster* operator[](uint id);
+	};
+
+	class P2EMonException : public std::exception {
+	private:
+		string _msg;
+		P2EMonExceptionCode _code;
+	public:
+		P2EMonException(P2EMonExceptionCode excode);
+		P2EMonException(P2EMonExceptionCode excode, const string& msg);
+
+		P2EMonExceptionCode code() const;
+		string msg() const;
+	};
+
+	// Each instance of a monsterW
+	class Monster {
+	protected:
+		Core* _core;
+		uint _id;
+		string _name;
+		umap<string, modval> _attrs;
+
+		void _init(Core* core, uint id);
+	public:
+		Monster(Core* core);
+		Monster(Core* core, const string& name);
+		Monster(Core* core, uint id);
+		Monster(Core* core, uint id, const string& name);
 
 		uint id() const;
 		string name() const;
 
 		void id(uint new_id);
-		string name(string& new_name);
+		void name(const string& new_name);
 	};
 }
 
---
+++ src/p2em_core.cpp
@@ -0,0 +1,76 @@
+
+#include "p2em_core.h"
+
+using namespace p2em_core;
+
+P2EMonException::P2EMonException(P2EMonExceptionCode excode, const string& msg) {
+	_code = excode;
+	_msg = msg;
+}
+
+P2EMonException::P2EMonException(P2EMonExceptionCode excode) : P2EMonException::P2EMonException(excode, P2EMonExceptionStrings[excode]) {} ;
+
+P2EMonExceptionCode P2EMonException::code() const { return _code; }
+string P2EMonException::msg() const { return _msg; }
+
+Core::Core() {
+	umap<uint, Monster*> _monsters = { {P2EMON_NULL_ID, nullptr} };
+}
+
+bool Core::has(uint id) const {
+	return (_monsters.find(id) != _monsters.end());
+}
+
+bool Core::has(const Monster* mon) const {
+	return has(mon->id());
+}
+
+Monster* Core::get(uint id) {
+	if (!has(id)) throw P2EMonException(P2EMON_ERR_MONSTER_NOT_FOUND);
+	return _monsters[id];
+}
+
+const Monster* Core::get(uint id) const {
+	return static_cast<const Monster*>(const_cast<Core*>(this)->operator[](id));
+}
+
+Monster* Core::operator[](uint id) { return get(id); }
+
+const Monster* Core::operator[](uint id) const { return get(id); }
+
+void Core::add(Monster* mon) {
+	// Check if monster is already in record
+	if (has(mon->id())) {
+		// If the monster is already in the record, do nothing; otherwise raise an exception
+		if (_monsters[mon->id()] == mon) return;
+		else throw P2EMonException(P2EMON_ERR_DUPLICATE_MONSTER);
+	}
+	else {
+		// Add the new monster
+		_monsters.insert({ mon->id(), mon });
+	}
+
+	// Should not reach this
+	throw P2EMonException(P2EMON_ERR);
+}
+
+void Core::remove(Monster* mon) {
+	// Check if monster is already in record
+	if (has(mon->id())) {
+		// If the monster matches the record, remove it; otherwise raise an exception
+		if (_monsters[mon->id()] == mon) _monsters.erase(mon->id());
+		else throw P2EMonException(P2EMON_ERR_MONSTER_MISMATCH);
+	}
+	else {
+		// If the monster is not in the record, do nothing
+		return;
+	}
+
+	// Should not reach this
+	throw P2EMonException(P2EMON_ERR);
+}
+
+void Core::remove(uint id) {
+	// If the monster is already in record, remove it. Otherwise do nothing
+	if (has(id)) remove(_monsters[id]);
+}
